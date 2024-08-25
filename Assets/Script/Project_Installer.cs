4118bede20ec02b677febe1f4a4d9057d55c1090
r Star <starlove124760@gmail.com>
2024-08-25 13:47:04 +0800

    import new texture

--- Assets/Script/Project_Installer.cs
+++ Assets/Script/Project_Installer.cs
@@ -1,9 +1,13 @@
 using UnityEngine;
+using Zenject;
 
 namespace DefaultNamespace
 {
-    public class Project_Installer : MonoBehaviour
+    public class Project_Installer : MonoInstaller
     {
-        
+        public override void InstallBindings()
+        {
+            Container.InstantiatePrefabResource("Music");
+        }
     }
 }
